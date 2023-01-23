7af3334a903308cb69bc18d5f1b8437c4f8c16c5
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-01-23 13:23:12 -0800

    pages created

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import { Component } from "solid-js";
+
+const Home:Component = () => {
+    return (
+        <div>Home</div>
+    )
+}
+
+export default Home
---
+++ src/pages/SavedRepos.tsx
@@ -0,0 +1,9 @@
+import { Component } from "solid-js";
+
+const SavedRepos:Component = () => {
+    return (
+        <div>Saved Repos</div>
+    )
+}
+
+export default SavedRepos
