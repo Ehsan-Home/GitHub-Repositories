4a8f56803964b6fb3303759036d5ba1df9c260be
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-01-23 13:22:12 -0800

    Nav component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import type { Component } from 'solid-js';
+import Nav from './components/Nav';
 
 
 const App: Component = () => {
   return (
     <div>
+      <Nav />
       <h1>Component</h1>
     </div>
   );
