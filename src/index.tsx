6e053b9687815f3e12f49f28d72c03e8f2e543ec
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-01-23 13:07:38 -0800

    bootstrap setup done

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,6 @@
 import { render } from 'solid-js/web';
 
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 render(() => <App />, document.getElementById('root') as HTMLElement);
