d330ac391ccae2d97cc54a936c5e964513bad519
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-01-23 17:53:56 -0800

    nav ui added and navlink added

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,26 @@
+import { NavLink } from "solid-app-router";
 import { Component } from "solid-js";
 
-const Nav:Component = () => {
-    return (
-        <div>Nav</div>
-    )
-}
+const Nav: Component = () => {
+  return (
+    <nav class="mt-5 mb-5">
+      <NavLink
+        href="/"
+        class="me-2 btn btn-primary"
+        activeClass="btn-success"
+        end
+      >
+        Home
+      </NavLink>
+      <NavLink
+        href="/savedrepos"
+        class="me-2 btn btn-primary"
+        activeClass="btn-success"
+      >
+        Saved repos ~ 5
+      </NavLink>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
