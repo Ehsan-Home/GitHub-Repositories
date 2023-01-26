9e9b6482e0cfc9a91c2500fba2f31757bb21d79e
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-01-25 18:28:55 -0800

    username search component developed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,23 @@
-import { Component } from "solid-js";
+import { Component, createEffect } from "solid-js";
+import { setUsername, repos } from "../App";
 
-const Home:Component = () => {
-    return (
-        <div>Home</div>
-    )
-}
+const Home: Component = () => {
+  const extractUserName = (event: Event) => {
+    event.preventDefault();
+    const usernameTextInput = document.getElementById(
+      "username"
+    ) as HTMLInputElement;
+    setUsername(usernameTextInput.value);
+  };
 
-export default Home
+  return (
+    <div>
+      <form class="mb-3" onSubmit={(event) => extractUserName(event)}>
+        <input id="username" type="text" class="p-1 align-middle" required />
+        <button class="ms-3 btn btn-dark">Search</button>
+      </form>
+    </div>
+  );
+};
+
+export default Home;
