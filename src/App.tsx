e3744d80e47d5dc9d8a6566e54244848e0e731fc
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-01-25 17:33:28 -0800

    API call implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
 import { Route, Routes } from "solid-app-router";
-import type { Component } from "solid-js";
+import { Component, createEffect, createSignal } from "solid-js";
 import Nav from "./components/Nav";
+import fetchRepos from "./networks/FetchRepos";
 import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
 import SavedRepos from "./pages/SavedRepos";
 
 const App: Component = () => {
+  const [username, SetUsername] = createSignal("Ehsan-Home");
+  const [repos, setRepos] = createSignal([]);
+
+  createEffect(async () => {
+    const data = await fetch(
+      `https://api.github.com/users/${username()}/repos`
+    );
+    setRepos(await data.json());
+  });
+
   return (
     <div class="container">
       <Nav />
       <Routes>
         <Route path="/" component={Home} />
         <Route path="/savedrepos" component={SavedRepos} />
-        {/* <Route path="*" component={NotFound} /> */}
       </Routes>
     </div>
   );
