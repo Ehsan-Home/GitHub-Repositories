1223aff697bad0c894a0eb713e0ad87796b0713c
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-01-27 09:31:14 -0800

    empty component UI improved

--- src/components/Empty.tsx
+++ src/components/Empty.tsx
@@ -1,7 +1,11 @@
 import { Component } from "solid-js";
 
 const Empty: Component = () => {
-  return <div>Empty</div>;
+  return (
+    <div class="border">
+      <h5 class="p-5 text-center">No repository found</h5>
+    </div>
+  );
 };
 
 export default Empty;
