3fa7da0c24cf10e8dee7f196f525f4eb1b3cf810
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-01-27 09:53:33 -0800

    spinner UI added

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,7 +1,13 @@
 import { Component } from "solid-js";
 
 const Spinner: Component = () => {
-  return <div>Spinning</div>;
+  return (
+    <div class="text-center">
+      <div class="spinner-grow text-primary" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  );
 };
 
 export default Spinner;
