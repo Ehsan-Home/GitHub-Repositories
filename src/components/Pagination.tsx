f978996c29e28b2ab386538d598f17bf2f02a28f
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-01-28 13:38:45 -0800

    href of next, previous and for added

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,36 +1,38 @@
 import { NavLink } from "solid-app-router";
 import { Component, For } from "solid-js";
+import { pageNumber } from "../pages/Home";
 
 const Pagination: Component = () => {
   return (
     <nav aria-label="Page navigation example">
       <ul class="pagination justify-content-center">
         <li class="page-item">
-          <NavLink
-            class="page-link"
-            href={`prev`}
-            tabindex="-1"
-            aria-disabled="true"
-          >
+          <NavLink class="page-link" href={`/${parseInt(pageNumber()) - 1}`}>
             Previous
           </NavLink>
         </li>
-        <For each={[1, 2, 3]}>
+        <For
+          each={[
+            parseInt(pageNumber()),
+            parseInt(pageNumber()) + 1,
+            parseInt(pageNumber()) + 2,
+          ]}
+        >
           {(i) => (
             <li class="page-item">
-              <NavLink class="page-link" href={`/${i}`}>
+              <NavLink
+                class={`page-link ${
+                  i === parseInt(pageNumber()) ? "active" : ""
+                }`}
+                href={`/${i}`}
+              >
                 {i}
               </NavLink>
             </li>
           )}
         </For>
         <li class="page-item">
-          <NavLink
-            class="page-link"
-            href={`next`}
-            tabindex="-1"
-            aria-disabled="true"
-          >
+          <NavLink class="page-link" href={`/${parseInt(pageNumber()) + 1}`}>
             Next
           </NavLink>
         </li>
