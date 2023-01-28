19a135bd96ce7211c92b1260fe44c4d34c68735c
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-01-28 09:14:55 -0800

    pagination UI added

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,7 +1,37 @@
 import { Component } from "solid-js";
 
 const Pagination: Component = () => {
-  return <div>Pagination</div>;
+  return (
+    <nav aria-label="Page navigation example">
+      <ul class="pagination justify-content-center">
+        <li class="page-item">
+          <a class="page-link" href="#" tabindex="-1" aria-disabled="true">
+            Previous
+          </a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="#">
+            1
+          </a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="#">
+            2
+          </a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="#">
+            3
+          </a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="#">
+            Next
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Pagination;
