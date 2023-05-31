03f703b123325b2ab40eeca5197f031ebd885736
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-05-31 14:02:29 -0700

    GitHub image added, and leader text changed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,29 @@
 import { Component } from "solid-js";
+import img from "../static/images/github.png";
 
 const Home: Component = () => {
   return (
     <>
-      <div class="text-center mt-5">
+      {/* <div class="text-center">
+        <i class="fa-brands fa-square-github fa-spin fa-2xl"></i>
+      </div> */}
+      <img
+        src={img}
+        class="mx-auto d-block mt-3"
+        width={200}
+        height={200}
+        alt="GitHub logo"
+      ></img>
+      <div class="text-center mt-3">
         <h1 class="display-1">GitHubVentory</h1>
       </div>
-      <p class="lead text-center">
-        The Ultimate Hub for Storing and Discovering Your Favorite GitHub
-        Repositories
-      </p>
+      <p class="lead text-center">👋🏻 Say goodbye to scattered bookmarks</p>
       <p class="text-center text-secondary">
         With countless repositories hosting innovative projects, it's no wonder
         developers find themselves lost in a sea of inspiring code bases. That's
         where GitHubVentory comes in—a powerful and user-friendly web
         application designed to revolutionize the way you store and explore your
-        favorite GitHub repositories. Say goodbye to scattered bookmarks and
-        hello to an organized, personalized hub that enhances your development
-        journey like never before.
+        favorite GitHub repositories.
       </p>
       <div class="d-flex justify-content-center">
         <div class="p-2">
